126627dfa034f08bc47ae90c3068df0c82e102fa
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2024-05-27 19:34:04 -0600

    Steps section finished
    Steps section finished, working on routing each one and save the image in a db called neon

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,9 +1,11 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { ReactNode } from "react";
+import { Steps } from "../../components/Steps";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <MaxWidthWrapper className="flex flex-1 flex-col">
+      <Steps />
       {children}
     </MaxWidthWrapper>
   );
